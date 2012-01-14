b23d1f35b10222740521a39524cebb5d04ffe328
Mateusz Ochtera <spaczn@gmail.com>
2012-01-14 14:20:12 +0100

    add entrance dimensions

--- lib/breeding/bus/entrance.rb
+++ lib/breeding/bus/entrance.rb
@@ -2,9 +2,18 @@ module Breeding
   module Bus
     class Entrance
     	attr_reader :x, :y
-
+    	
     	def initialize(x, y)
     		@x, @y = x, y
+    		@@width, @@length = 10, 15 
+    	end
+
+    	def width
+    		@@width
+    	end
+
+    	def length
+    		@@length
     	end
     end
   end
